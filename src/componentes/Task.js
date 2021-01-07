4c1cfe1782d2b1700f17e8edd09e9d37e8de540c
Abraham0912 <09120912ahn@gmail.com>
2021-01-07 17:17:44 -0600

    addPropTypes

--- src/componentes/Task.js
+++ src/componentes/Task.js
@@ -1,15 +1,43 @@
 import React,{Component} from 'react';
 import './Tasks.css';
+import PropTypes from 'prop-types';
 
 
 class Task extends Component{
+    StyleCompleted(){
+        return{
+            fontSize:'20px',
+            color: this.props.task.done ? 'gray': 'black',
+            textDecoration: this.props.task.done ? 'line-through' : 'none'
+        }
+    }
+
+
     render(){
-        return <p className="red">
-        {this.props.task.title} - {this.props.task.description} - {this.props.task.done} - {this.props.task.id}
+        const {task} = this.props;
+
+        return <p style={this.StyleCompleted()}>
+        {task.title} - 
+        {task.description} - 
+        {task.done} - 
+        {task.id}
         <input type="checkbox"></input>
-        <button>x</button>
+        <button style = {btnDelete}>x</button>
     </p>
     }
+}
+
+Task.propTypes = {
+    task: PropTypes.object.isRequired
+}
+const btnDelete = {
+    fontSize:'18px',
+    background: '#ea2027',
+    color: '#fff',
+    border: 'none',
+    padding: '10px 15px',
+    borderRadius : '50%',
+    cursor: 'pointer',
 }
 
 export default Task;
